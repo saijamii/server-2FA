0759bf13ef18787ec655bfe3c5506e59b8f34523
saijami <saijami38@gmail.com>
2025-05-01 21:58:48 +0530

    logs removed

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -23,9 +23,6 @@ router.get("/status", authStatus); // Auth Status Route
 router.post(
   "/2fa/setup",
   (req, res, next) => {
-    console.log("Is Authenticated:", req.isAuthenticated?.());
-    console.log("Session:", req.session);
-    console.log("User:", req.user);
     if (req.isAuthenticated()) {
       return next();
     }
